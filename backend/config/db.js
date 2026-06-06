e65ab6363839a5fe1810bd0342fa011f856af001
tanmay-deev <tanmaybonde10@gmail.com>
2026-06-06 17:00:12 +0530

    mongo db url changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://tanmaybonde20_db_user:tanmay2006@cluster0.vf0axal.mongodb.net/RealState").then(() => {
+    await mongoose.connect(process.env.MONGODB_URL).then(() => {
         console.log("DB Connected"); 
     });
 }
---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,9 @@
+const sendEmail = async (options) => {
+    try {
+        const BREVO_API_KEY = process.env.BREVO_API_KEY?.trim();
+    }
+    
+    catch (error) {
+        
+    }
+}
